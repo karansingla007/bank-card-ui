c9a64af6c0a14b1941d8f49ef839852c44c12057
Kara Singla <karansingla120@gmail.com>
2023-02-10 22:19:21 -0500

    Bank card ui animation

---
+++ lib/data/card.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:bank_cards_ui/state.dart';
+import 'package:bank_cards_ui/utils/colors.dart';
+
+class CardModel {
+  final double balance;
+
+  final List<Color> colors;
+
+  final int id;
+
+  CardModel(
+    this.id,
+    this.balance,
+    this.colors,
+  );
+
+  @override
+  String toString() {
+    return 'CardModel{balance: $balance, id: $id}';
+  }
+}
+
+List<CardModel> cards = List.generate(
+        AppColors.gradientColors.length,
+        (index) => CardModel(
+            index + 1, (index + 1) * 125, AppColors.gradientColors[index]))
+    .toList();
+
+extension CardsExt on List<CardModel> {
+  bool isSelectedCard(AppState appState, int index) {
+    return appState.currentCard != null && appState.currentCard != this[index];
+  }
+}
